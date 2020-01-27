b5c34ad7395227b5c2da939490df16a54887b5e0
ZastrixArundell <lukabodroza@gmail.com>
2020-01-27 18:58:53 +0100

    Sending messages to the Arduino

--- lib/arduiner/servers/serial_port_server.ex
+++ lib/arduiner/servers/serial_port_server.ex
@@ -1,17 +1,25 @@
 defmodule Arduiner.Servers.SerialPortServer do
-    use GenServer
-    alias Circuits.UART, as: Port
+  use GenServer
+  alias Circuits.UART, as: Port
 
-    def start_link(port), do: GenServer.start_link(__MODULE__, port)
+  def start_link(port), do: GenServer.start_link(__MODULE__, port, name: __MODULE__)
 
-    def init(port) do
-        IO.write "Starting UART GenServer\n"
-        {:ok, pid} = Port.start_link
-        IO.write "Started UART GenServer on "; IO.inspect pid;
-        IO.write "Starting serial on #{port}\n"
-        Port.open(pid, port, speed: 9600, active: false)
-        IO.write "Started serial on #{port}\n"
-        {:ok, {port, pid}}
-    end
+  def init(port) do
+    IO.write "Starting UART GenServer\n"
+    {:ok, pid} = Port.start_link
+    IO.write "Started UART GenServer on "; IO.inspect pid;
+    IO.write "Starting serial on #{port}\n"
+    Port.open(pid, port, speed: 9600, active: false)
+    IO.write "Started serial on #{port}\n"
+    {:ok, {port, pid}}
+  end
+
+  def write_message_to_port(message), do: GenServer.cast(__MODULE__, {:send_message, message})
+
+  def handle_cast({:send_message, message}, state) do
+    {port, pid} = state
+    Port.write(pid, message)
+    {:noreply, state}
+  end
 
 end
