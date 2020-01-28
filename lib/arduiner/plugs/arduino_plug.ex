fc80290ce7923afbce917b134fd874845b964bd0
ZastrixArundell <lukabodroza@gmail.com>
2020-01-28 10:53:17 +0100

    Added docs to most important part

--- lib/arduiner/plugs/arduino_plug.ex
+++ lib/arduiner/plugs/arduino_plug.ex
@@ -3,10 +3,22 @@ defmodule Arduiner.Plugs.ArduinoPlug do
   alias Phoenix.Controller
   alias ArduinerWeb.Router.Helpers, as: Routes
 
+  @moduledoc """
+  This is the plug module which checks whether the Arduino is connected and 
+  redirects to the connect page if it is not. 
+
+  It uses a specific atom either `:usage` or `:create`.
+
+  `Usage` is when the pages are where you control the Arduino and `Create` is only
+  for the page where you connected to the Arduino intially.
+  """
+
   def init(default), do: default
 
+  @doc """
+  If the Arduino is not connected redirect to the connect page.
+  """
   def call(conn, :usage) do
-    IO.write "Called usage"
     if !Arduiner.Servers.SerialPortServer.get_port do
       conn
       |> Controller.put_flash(:error, "You need to connect to an Arduino first!")
@@ -17,8 +29,10 @@ defmodule Arduiner.Plugs.ArduinoPlug do
     end
   end
 
+  @doc """
+  If the arduino is connected, you can't access the page to be connected again.
+  """
   def call(conn, :create) do
-    IO.write "Called create"
     if !!Arduiner.Servers.SerialPortServer.get_port do
       conn
       |> Controller.put_flash(:error, "You need to disconnect first!")
--- lib/arduiner/servers/serial_port_server.ex
+++ lib/arduiner/servers/serial_port_server.ex
@@ -2,27 +2,52 @@ defmodule Arduiner.Servers.SerialPortServer do
   use GenServer
   alias Circuits.UART, as: Port
 
+  @moduledoc """
+  A GenServer which represents the connection to the Arduino. The GenServer itself is always active
+  as it starts a child GenServer for the Port communication. 
+  """
+
   # Client side API
 
+  @doc false
   def start_link(_), do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
+  @doc """
+  This lets you start the communication to the port. The first argument is the name of the port
+  and the second argument is the baud rate of the port. 
+
+  It will return either `:ok` or `:error` depending on the situation.
+  """
   def connect_to_port(port, rate), do: GenServer.call(__MODULE__, {:connect, port, rate})
 
+  @doc """
+  This lets you send a string to the port. It will either reply with `:ok` or `:error`.
+  """
   def write_message_to_port(message), do: GenServer.call(__MODULE__, {:send_message, message})
 
+  @doc """
+  This returns the name of the conncted port.
+  """
   def get_port, do: GenServer.call(__MODULE__, {:get_port})
 
+  @doc """
+  This stops the child port GenServer but not the SerialPortServer. You can't use the 
+  `connect_to_port/2` at a later time again.
+  """
   def stop, do: GenServer.cast(__MODULE__, {:stop_port})
 
   # Server code
 
+  @doc false
   def init(_), do: {:ok, %{port: nil, pid: nil}}
 
+  @doc false
   def handle_cast({:stop_port}, state) do
     Port.close(state.pid)
     {:noreply, %{port: nil, pid: nil}}
   end
 
+  @doc false
   def handle_call({:send_message, message}, _from, state) do
     if Port.write(state.pid, message) == :ok do
       {:reply, :ok, state}
@@ -31,6 +56,7 @@ defmodule Arduiner.Servers.SerialPortServer do
     end
   end
 
+  @doc false
   def handle_call({:connect, port, rate}, _from, state) do
 
     state =
@@ -54,6 +80,7 @@ defmodule Arduiner.Servers.SerialPortServer do
     end
   end
 
+  @doc false
   def handle_call({:get_port}, _from, state), do: {:reply, state.port, state}
 
 end
